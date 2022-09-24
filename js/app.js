d2aea4548beae3842bf6d3b4e3eddcc533a25286
Kacper Mikolajczyk <vkacperpl@gmail.com>
2022-09-25 00:59:13 +0200

    Added blog and some more buttons

--- js/app.js
+++ js/app.js
@@ -7,11 +7,15 @@ const switcher = () => {
         document.getElementById('white_switch').style.visibility = 'hidden'; 
         document.getElementById('black_switch').style.visibility = 'visible';
         document.getElementById('main-title').style.color = '#222426';
+        document.getElementById('blog-topic').style.color = '#222426';
+        document.getElementById('blog-description').style.color = '#222426';
     } else if (color === 'white') {
         color = 'dark';
         document.body.style.backgroundColor = '#222426';
         document.getElementById('white_switch').style.visibility = 'visible';
         document.getElementById('black_switch').style.visibility = 'hidden';
         document.getElementById('main-title').style.color = 'white';
+        document.getElementById('blog-topic').style.color = 'white';
+        document.getElementById('blog-description').style.color = 'white';
     }
 };
