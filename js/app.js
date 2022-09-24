2d2fe5c8f6ea6215127ddc814e5cb666f8ca0c81
Kacper Mikolajczyk <vkacperpl@gmail.com>
2022-09-25 00:28:25 +0200

    Added switcher dark/light to the website

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
-const nameClick = () => {
-    location.href = 'https://github.com/KacpersDev';
+let color = 'dark';
+
+const switcher = () => {
+    if (color === 'dark') {
+        color = 'white';
+        document.body.style.backgroundColor = 'white';
+        document.getElementById('white_switch').style.visibility = 'hidden'; 
+        document.getElementById('black_switch').style.visibility = 'visible';
+        document.getElementById('main-title').style.color = '#222426';
+    } else if (color === 'white') {
+        color = 'dark';
+        document.body.style.backgroundColor = '#222426';
+        document.getElementById('white_switch').style.visibility = 'visible';
+        document.getElementById('black_switch').style.visibility = 'hidden';
+        document.getElementById('main-title').style.color = 'white';
+    }
 };
