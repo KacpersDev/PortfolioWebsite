7accd73bc00eb4cf59a2b4aef32ab1bf9d806eaa
Kacper Mikolajczyk <vkacperpl@gmail.com>
2022-09-25 02:04:41 +0200

    Added about me && skills

--- js/app.js
+++ js/app.js
@@ -7,8 +7,9 @@ const switcher = () => {
         document.getElementById('white_switch').style.visibility = 'hidden'; 
         document.getElementById('black_switch').style.visibility = 'visible';
         document.getElementById('main-title').style.color = '#222426';
-        document.getElementById('blog-topic').style.color = '#222426';
-        document.getElementById('blog-description').style.color = '#222426';
+        document.getElementById('about-title').style.color = '#222426';
+        document.getElementById('about-description').style.color = '#222426';
+        document.getElementById('skills-title').style.color = '#222426';
     } else if (color === 'white') {
         color = 'dark';
         document.body.style.backgroundColor = '#222426';
@@ -17,5 +18,8 @@ const switcher = () => {
         document.getElementById('main-title').style.color = 'white';
         document.getElementById('blog-topic').style.color = 'white';
         document.getElementById('blog-description').style.color = 'white';
+        document.getElementById('about-title').style.color = 'white';
+        document.getElementById('about-description').style.color = 'white';
+        document.getElementById('skills-title').style.color = 'white';
     }
 };
