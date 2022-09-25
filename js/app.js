95a85d6e2dad20093515fd13c648c5931ff67dda
Kacper Mikolajczyk <vkacperpl@gmail.com>
2022-09-25 19:10:52 +0200

    Website has been done

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 let color = 'dark';
+let sColor = 'dark';
 let clicked = false;
 
 const switcher = () => {
@@ -8,9 +9,10 @@ const switcher = () => {
         document.getElementById('white_switch').style.visibility = 'hidden'; 
         document.getElementById('black_switch').style.visibility = 'visible';
         document.getElementById('about-title').style.color = '#222426';
-        document.getElementById('about-description').style.color = '#222426'
-        document.getElementById('projects-title').style.color = '#222426'
+        document.getElementById('about-description').style.color = '#222426';
+        document.getElementById('projects-title').style.color = '#222426';
         document.getElementById('projects-description').style.color = '#222426';
+        document.getElementById('skills-title').style.color = '#222426';
     } else if (color === 'white') {
         color = 'dark';
         document.body.style.backgroundColor = '#222426';
@@ -20,8 +22,21 @@ const switcher = () => {
         document.getElementById('about-description').style.color = 'white'
         document.getElementById('projects-title').style.color = 'white'
         document.getElementById('projects-description').style.color = 'white';
+        document.getElementById('skills-title').style.color = 'white';
     }
 };
+
+const colorSwitch = () => {
+    if (sColor === 'dark') {
+        sColor = 'white';
+        document.body.style.backgroundColor = 'white';
+        document.getElementById('contact-title').style.color = '#222426';
+    } else if (sColor === 'white') {
+        sColor = 'dark';
+        document.body.style.backgroundColor = '#222426';
+        document.getElementById('contact-title').style.color = 'white';
+    }
+}
 
 const barClick = () => {
     if (!clicked) {
@@ -44,7 +59,6 @@ const resize = () => {
 window.onresize = resize;
 
 const submitButton = () => {
-    console.log(document.getElementById('name').value);
     document.getElementById('name').value = '';
     document.getElementById('email').value = '';
     document.getElementById('textarea').value = '';
